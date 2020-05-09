5af44ba692cb5c16d6de69082b109841c2080af4
Joshua Gomes <gomesjoshua@gmail.com>
2020-05-08 22:32:25 -0400

    Added validate positive integer input file - Added comments

--- validate_number_input.cpp
+++ validate_number_input.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+// *************************************************************
+// Function to test for positive integer input in C++ code
+// *************************************************************
 bool validate_input(string str) {
 		// Check user input for non-numeric values
 		if (str.end() != find_if_not(str.begin(), str.end(), ::isdigit)) {
@@ -16,6 +19,14 @@ bool validate_input(string str) {
 			return true;
 }
 
+
+// *************************************************************
+// Example code for demonstration:
+//	This function asks the user for a positive number and prints
+//	the number to the console.
+//	The function also converts the string input to an int before
+//	returning it.
+// *************************************************************
 int getNumber() {
 	int validNumber;
 	string num;
@@ -36,6 +47,7 @@ int getNumber() {
 	return validNumber;
 }
 
+// Driver code for testing
 int main() {
 	int user_num = getNumber();
 	cout << "Your number is: " << user_num << endl;
