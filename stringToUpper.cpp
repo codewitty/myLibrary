9ae88cf850d8f291523d4815f6bf3e74b6d852cf
Joshua G <gomesjoshua@gmail.com>
2020-06-02 12:01:28 -0700

    Create stringToUpper.cpp

---
+++ stringToUpper.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Function to convert a letter to uppercase
+int	ft_toupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	else
+		return (c);
+}
+
+// Function to convert a string to uppercase
+string toUpperString(string in){
+	for(int i = 0; i < in.length(); ++i) {
+		in[i] = ft_toupper(in[i]);
+	}
+	return in;
+}
+
+// Example driver code
+
+int main() {
+	string str = "Hello";
+	cout << toUpperString(str);
+}
